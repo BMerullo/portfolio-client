ae2070eb758657f137fd9b0c48d06b5e4fbefcf6
Bob Merullo <bmerullo85@gmail.com>
2022-10-05 14:50:59 -0600

    bug fix

--- src/pages/AdminPortal.jsx
+++ src/pages/AdminPortal.jsx
@@ -15,7 +15,7 @@ const AdminPortal = (props) => {
     e.preventDefault();
     axios
       .post(
-        "https://bob-merullo-server.herokuapp.com/api/logout",
+        "https://bob-merullo-server.herokuapp.com/api/users/logout",
         {},
         {
           withCredentials: true,
